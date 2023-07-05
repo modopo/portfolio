08f1e3f8a2d1afe5a7d89329f8efcb5e1dd3cb69
Jonathan Tsai <modopo@gmail.com>
2023-07-05 15:42:25 -0700

    menu icon rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App() {
-  return (
-    <div className="App">
+import Navbar from "./scenes/Navbar";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
+function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMedScreen = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { ReactComponent as MenuIcon } from "../assets/menu-icon.svg";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+        hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  )
+}
+
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveSmallScreen = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">JT</h4>
+
+        {isAboveSmallScreen ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contacts"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <MenuIcon alt="menu-icon" />
+          </button>
+        )}
+
+        {!isAboveSmallScreen && isMenuToggled && (
+          <div className="fixed rigth-0 bottom-0 h-full bg-blue w-[300px]">
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="menu" src="../assets/menu-svgrepo-com.svg" />
+              </button>
+            </div>
+          </div>
+        )}
+
+      </div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
